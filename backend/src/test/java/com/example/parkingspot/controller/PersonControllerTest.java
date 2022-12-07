1bf6c9d41a903e4c3f1c05316b08bad329a2496b
Christerresin <christer.resin@gmail.com>
2022-12-07 14:07:35 +0100

    refactor to use annotaion ExtendsWith and Mock annotations

--- backend/src/test/java/com/example/parkingspot/controller/PersonControllerTest.java
+++ backend/src/test/java/com/example/parkingspot/controller/PersonControllerTest.java
@@ -3,17 +3,24 @@ package com.example.parkingspot.controller;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.*;
 
 import com.example.parkingspot.entity.Person;
 import com.example.parkingspot.service.PersonService;
 
+@ExtendWith(MockitoExtension.class)
 public class PersonControllerTest {
+
+  @Mock
+  PersonService service;
+
   @Test
   void getAllPersons_shouldReturnListOfPersons() {
-    PersonService service = Mockito.mock(PersonService.class);
     Person p1 = new Person();
     p1.setFirstName("Test");
     p1.setLastName("Testson");
