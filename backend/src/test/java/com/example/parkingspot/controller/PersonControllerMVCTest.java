d6e5a8fe32190210f69aa1b5a76e1d60a025b9c0
Christerresin <christer.resin@gmail.com>
2022-12-07 20:14:11 +0100

    add test for endpoint getPersons 200 ok

--- backend/src/test/java/com/example/parkingspot/controller/PersonControllerMVCTest.java
+++ backend/src/test/java/com/example/parkingspot/controller/PersonControllerMVCTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.example.parkingspot.service.PersonService;
 
@@ -18,8 +20,9 @@ public class PersonControllerMVCTest {
   private MockMvc mockMvc;
 
   @Test
-  void callingEndpointGetPersons_shouldReturn200OK() {
-
+  void callingEndpointGetPersons_shouldReturn200OK() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/api/persons"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
   }
 
 }
