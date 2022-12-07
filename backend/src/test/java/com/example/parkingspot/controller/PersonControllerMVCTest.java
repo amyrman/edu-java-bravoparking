adb674a6ad325080b778cabef871171c03f1aa17
Christerresin <christer.resin@gmail.com>
2022-12-07 20:40:54 +0100

    add test for endpoint getPerson by id, return 200 ok

--- backend/src/test/java/com/example/parkingspot/controller/PersonControllerMVCTest.java
+++ backend/src/test/java/com/example/parkingspot/controller/PersonControllerMVCTest.java
@@ -1,5 +1,7 @@
 package com.example.parkingspot.controller;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.example.parkingspot.entity.Person;
 import com.example.parkingspot.service.PersonService;
 
 @WebMvcTest(PersonController.class)
@@ -21,11 +24,24 @@ public class PersonControllerMVCTest {
   private MockMvc mockMvc;
 
   @Test
-  void callingEndpointGetPersons_shouldReturn200OK() throws Exception {
+  void callingEndpointGetPersons_shouldReturnJsonAnd200OK() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get("/api/persons"))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.content().json("[]"));
+  }
+
+  @Test
+  void callingEndpointGetPerson_shouldReturnJsonAnd200OK() throws Exception {
+    Person person = new Person();
+    person.setFirstName("Test");
+    person.setLastName("Efternamn");
+    person.setId(1);
+
+    when(service.getPersonById(1L)).thenReturn(person);
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/api/persons/{id}", 1L))
+        .andExpect(MockMvcResultMatchers.status().isOk());
   }
 
 }
