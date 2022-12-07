2535b309c483c93c018b98b1f745468d12e4b684
Christerresin <christer.resin@gmail.com>
2022-12-07 20:17:08 +0100

    refactor getPersons test to check if content type is json

--- backend/src/test/java/com/example/parkingspot/controller/PersonControllerMVCTest.java
+++ backend/src/test/java/com/example/parkingspot/controller/PersonControllerMVCTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,7 +23,8 @@ public class PersonControllerMVCTest {
   @Test
   void callingEndpointGetPersons_shouldReturn200OK() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get("/api/persons"))
-        .andExpect(MockMvcResultMatchers.status().isOk());
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
   }
 
 }
