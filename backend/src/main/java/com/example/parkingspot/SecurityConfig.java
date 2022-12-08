9eb55e863aa290f9e8b2226ba4e04b111554a21b
Anders Myrman <amyhrman@gmail.com>
2022-12-08 21:34:57 +0100

    Add request handlers and JWT functions

--- backend/src/main/java/com/example/parkingspot/SecurityConfig.java
+++ backend/src/main/java/com/example/parkingspot/SecurityConfig.java
@@ -2,7 +2,12 @@ package com.example.parkingspot;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,14 +16,29 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    
     return http
               .csrf().disable()
               .cors().disable()
               .formLogin().disable()
               .logout().disable()
               .sessionManagement().disable()
-
-    .build();
+              .httpBasic().disable()
+              .authorizeHttpRequests()
+                // TODO: /persons still returns 403? 
+                .requestMatchers(HttpMethod.GET, "/persons").hasAuthority("SCOPE_read:persons")
+                .requestMatchers(HttpMethod.GET, "/persons/**").permitAll()
+                .anyRequest().denyAll()
+              .and()
+              .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+              .build();
   }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder
+      .withJwkSetUri("https://fungover.org/auth/.well-known/jwks.json")
+      .jwsAlgorithm(SignatureAlgorithm.ES256)
+      .build();
+  }
+
 }
