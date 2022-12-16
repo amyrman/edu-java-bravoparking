5af0fe127f9916c80fee11208d1baa5cd9ea8ab8
Christerresin <christer.resin@gmail.com>
2022-12-16 14:12:54 +0100

    add input on cars page. makes post req on API to add car

--- frontend/src/component/cars.js
+++ frontend/src/component/cars.js
@@ -6,17 +6,43 @@ import { checkJwtExpired } from '../checkJwtExpired';
 import '../css/cars.css';
 
 function Cars() {
+  const initialValues = {
+    registration: '',
+  };
   const [cars, setCars] = useState();
+  const [registration, setRegistration] = useState('');
+  const userId = localStorage.getItem('userId');
+
+  const handleAddCar = (event) => {
+    event.preventDefault();
+
+    axios({
+      method: 'post',
+      url: `http://localhost:8080/api/cars`,
+      data: {
+        registration: registration,
+        person: {
+          userId: userId,
+        },
+      },
+    });
+  };
+
+  const handleInputChange = (e) => {
+    setRegistration(e.target.value);
+  };
 
   const handleGetCars = () => {
-    const userId = localStorage.getItem('userId');
     const token = localStorage.getItem('token');
 
     checkJwtExpired();
     setAuthToken(token);
 
     axios
-      .get(`http://localhost:8080/api/persons/${userId}/cars`)
+      .get(
+        `http://localhost:8080/api/persons/b1fcb6f5-a2ec-4c16-9c09-697233bacbab/cars`
+      )
+      // .get(`http://localhost:8080/api/persons/${userId}/cars`)
       .then((response) => {
         setCars(response.data);
       })
@@ -33,18 +59,26 @@ function Cars() {
     <div>
       <h1>CARS PAGE</h1>
       {cars ? (
-        <div>
-          <ul>
-            {cars.map((car) => (
-              <li key={car.id} className='cars_list_item'>
-                {car.registration}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul>
+          {cars.map((car) => (
+            <li key={car.id} className='cars_list_item'>
+              {car.registration}
+            </li>
+          ))}
+        </ul>
       ) : (
         <div></div>
       )}
+
+      <form onSubmit={handleAddCar}>
+        <input
+          type='text'
+          name='registration'
+          placeholder='Car registration'
+          onChange={handleInputChange}
+        />
+        <input type='submit' value='ADD CAR' />
+      </form>
     </div>
   );
 }
