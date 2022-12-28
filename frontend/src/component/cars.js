0d5e8b3e01a5dfaac761e6a993429a0576027505
Emil Noren <stratosfloor@hotmail.com>
2022-12-28 20:34:58 +0100

    set up selection box for starting new parking

--- frontend/src/component/cars.js
+++ frontend/src/component/cars.js
@@ -1,87 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { setAuthToken } from '../setAuthToken';
-import { checkJwtExpired } from '../checkJwtExpired';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { setAuthToken } from "../setAuthToken";
+import { checkJwtExpired } from "../checkJwtExpired";
 
-import '../css/cars.css';
+import "../css/cars.css";
 
 function Cars() {
-  const [cars, setCars] = useState();
-  const [registration, setRegistration] = useState('');
-  const userId = localStorage.getItem('userId');
+	const [cars, setCars] = useState();
+	const [registration, setRegistration] = useState("");
+	const userId = localStorage.getItem("userId");
 
-  const handleAddCar = (event) => {
-    event.preventDefault();
-    setCars([...cars, { registration: registration }]);
+	const handleAddCar = (event) => {
+		event.preventDefault();
+		setCars([...cars, { registration: registration }]);
 
-    axios({
-      method: 'post',
-      url: `http://localhost:8080/api/cars`,
-      data: {
-        registration: registration,
-        person: {
-          userId: userId,
-        },
-      },
-    });
-    setRegistration('');
-  };
+		axios({
+			method: "post",
+			url: `http://localhost:8080/api/cars`,
+			data: {
+				registration: registration,
+				person: {
+					userId: userId,
+				},
+			},
+		});
+		setRegistration("");
+	};
 
-  const handleInputChange = (e) => {
-    setRegistration(e.target.value);
-  };
+	const handleInputChange = (e) => {
+		setRegistration(e.target.value);
+	};
 
-  const handleGetCars = () => {
-    const token = localStorage.getItem('token');
+	const handleGetCars = () => {
+		const token = localStorage.getItem("token");
 
-    checkJwtExpired();
-    setAuthToken(token);
+		checkJwtExpired();
+		setAuthToken(token);
 
-    axios
-      .get(`http://localhost:8080/api/persons/${userId}/cars`)
-      .then((response) => {
-        setCars(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+		axios
+			.get(`http://localhost:8080/api/persons/${userId}/cars`)
+			.then((response) => {
+				setCars(response.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  const renderCarsList = () => {
-    return cars ? (
-      <ul>
-        {cars.map((car) => (
-          <li key={car.registration} className='cars_list_item'>
-            {car.registration}
-          </li>
-        ))}
-      </ul>
-    ) : (
-      <div></div>
-    );
-  };
+	const renderCarsList = () => {
+		return cars ? (
+			<ul>
+				{cars.map((car) => (
+					<li key={car.registration} className="cars_list_item">
+						{car.registration}
+					</li>
+				))}
+			</ul>
+		) : (
+			<div></div>
+		);
+	};
 
-  useEffect(() => {
-    handleGetCars();
-  }, []);
+	useEffect(() => {
+		handleGetCars();
+	}, []);
 
-  return (
-    <div>
-      <h1>CARS PAGE</h1>
-      {renderCarsList()}
+	return (
+		<div>
+			<h1>CARS PAGE</h1>
+			{renderCarsList()}
 
-      <form onSubmit={handleAddCar}>
-        <input
-          type='text'
-          name='registration'
-          placeholder='Car registration'
-          onChange={handleInputChange}
-          value={registration}
-        />
-        <input type='submit' value='ADD CAR' />
-      </form>
-    </div>
-  );
+			<form onSubmit={handleAddCar}>
+				<input
+					type="text"
+					name="registration"
+					placeholder="Car registration"
+					onChange={handleInputChange}
+					value={registration}
+				/>
+				<input type="submit" value="ADD CAR" />
+			</form>
+		</div>
+	);
 }
 
 export default Cars;
--- frontend/src/component/events.js
+++ frontend/src/component/events.js
@@ -1,50 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "../css/events.css";
 
 function Events() {
-  const [events, setEvents] = useState([]);
+	const [events, setEvents] = useState([]);
+	const [zones, setZones] = useState([]);
+	const [cars, setCars] = useState([]);
 
-  const renderEventsList = (events) => {
-    return (
-      <div>
-        <ul>
-          {events.map((event) => (
-            <li key={event.id}>
-              <p>ParkingZone: {event.zone.name}</p>
-              <p>{event.active ? event.start : ''}</p>
-              <p>{event.active ? event.stop : ''}</p>
-              <p>Status: {event.active ? 'active' : 'finished'}</p>
-              <p>Registration: {event.car.registration}</p>
-              <p>---</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
+	const renderStartNewPark = (zones, cars) => {
+		return (
+			<div className="startEvent">
+				<h3>Start new parking</h3>
+				<form>
+					<label for="zones" id="zones"></label>
+					<select id="zones" name="zones">
+						{zones.map((zone) => (
+							<option key={zone.id}>{zone.name}</option>
+						))}
+					</select>
+				</form>
+				<form>
+					<label for="cars" id="cars"></label>
+					<select id="cars" name="cars">
+						{cars.map((car) => (
+							<option
+								key={car.registration}
+								value={car.registration}
+							>
+								{car.registration}
+							</option>
+						))}
+					</select>
+				</form>
+			</div>
+		);
+	};
 
-  async function handleGetEvents() {
-    const res = await axios
-      .get(`http://localhost:8080/api/events`)
-      .then((response) => {
-        return response;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    setEvents(res.data);
-  }
+	const renderEventsList = (events) => {
+		return (
+			<div>
+				<ul>
+					{events.map((event) => (
+						<li
+							key={event.id}
+							className={event.active ? "active" : "finished"}
+						>
+							<p>ParkingZone: {event.zone.name}</p>
+							<p>{event.active ? event.start : ""}</p>
+							<p>{event.active ? event.stop : ""}</p>
+							<p>
+								Status: {event.active ? "active" : "finished"}
+							</p>
+							<p>Registration: {event.car.registration}</p>
+							<p>---</p>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	};
 
-  useEffect(() => {
-    handleGetEvents();
-  }, []);
+	async function handleGetEvents() {
+		const res = await axios
+			.get(`http://localhost:8080/api/events`)
+			.then((response) => {
+				return response;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		setEvents(res.data);
+	}
 
-  return (
-    <div>
-      <h1>EVENTS PAGE</h1>
-      {renderEventsList(events)}
-    </div>
-  );
+	async function handleGetZones() {
+		const res = await axios
+			.get(`http://localhost:8080/api/zones`)
+			.then((response) => {
+				return response;
+			})
+			.catch((error) => console.log(error));
+		setZones(res.data);
+	}
+
+	async function handleGetCars() {
+		const userId = localStorage.getItem("userId");
+		const res = await axios
+			.get(`http://localhost:8080/api/persons/${userId}/cars`)
+			.then((response) => {
+				return response;
+			})
+			.catch((err) => console.log(err));
+
+		setCars(res.data);
+		//console.log(res.data);
+	}
+
+	useEffect(() => {
+		handleGetEvents();
+		handleGetZones();
+		handleGetCars();
+	}, []);
+
+	return (
+		<div>
+			<h1>EVENTS PAGE</h1>
+			{renderStartNewPark(zones, cars)}
+			<h3>History</h3>
+			{renderEventsList(events)}
+		</div>
+	);
 }
 
 export default Events;
