ad4bafd569bd55bdee4ee970a13de5c83b2936b1
Emil Noren <stratosfloor@hotmail.com>
2022-12-28 11:55:39 +0100

    Show name and email of logged in user on welcome page

--- frontend/src/component/home.js
+++ frontend/src/component/home.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { checkJwtExpired } from "../checkJwtExpired";
+import { setAuthToken } from "../setAuthToken";
+
+import "../css/home.css";
 
 function Home() {
-  return (
-    <div>
-      <h2>THIS IS THE PROFILE/PERSON PAGE</h2>
-    </div>
-  );
-}
+	const [userName, setUserName] = useState();
+	const [userEmail, setUserEmail] = useState();
 
+	const fetchUser = () => {
+		checkJwtExpired();
+		setAuthToken(localStorage.getItem("token"));
+		//const userid = localStorage.getItem("userId");
+		//console.log(userid);
+		axios
+			.get(
+				`http://localhost:8080/api/persons/${localStorage.getItem(
+					"userId"
+				)}`
+			)
+			.then((response) => {
+				setUserName(
+					response.data.firstName + " " + response.data.lastName
+				);
+				setUserEmail(response.data.email);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
+	return (
+		<div>
+			<h3>Welcome back, {userName}</h3>
+			<h4>
+				Your email adress is:
+				<p>{userEmail}</p>
+			</h4>
+		</div>
+	);
+}
 export default Home;
